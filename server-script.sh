1d1cf092903827cb9f00ecaf5173ca8f8000c8db
Induja Kodavati <125634414+indujakodavati@users.noreply.github.com>
2025-06-24 12:06:32 +0530

    Update server-script.sh

--- server-script.sh
+++ server-script.sh
@@ -8,7 +8,7 @@ then
     cd /home/ec2-user/addressbook
     # git pull origin demo1
 else
-    git clone https://github.com/induja2609/addressbook.git
+    git clone https://github.com/indujakodavati/addressbook.git
 fi
 
 cd /home/ec2-user/addressbook
